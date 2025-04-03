bcdb356919b42ceca402dfe32bb359e4d00690d0
SergioMartin86 <eienburuu@gmail.com>
2025-04-03 18:55:19 +0200

    Removing retro achievements dep

--- Core/SaveState.cpp
+++ Core/SaveState.cpp
@@ -48,7 +48,6 @@
 #include "Core/MemMap.h"
 #include "Core/MIPS/MIPS.h"
 #include "Core/MIPS/JitCommon/JitBlockCache.h"
-#include "Core/RetroAchievements.h"
 #include "HW/MemoryStick.h"
 #include "GPU/GPUState.h"
 
@@ -402,7 +401,6 @@ double g_lastSaveTime = -1.0;
 		currentMIPS->DoState(p);
 		HLEDoState(p);
 		__KernelDoState(p);
-		Achievements::DoState(p);
 		// Kernel object destructors might close open files, so do the filesystem last.
 		pspFileSystem.DoState(p);
 	}
@@ -411,14 +409,6 @@ double g_lastSaveTime = -1.0;
 	{
 		if (!NetworkAllowSaveState()) {
 			return;
-		}
-		if (Achievements::HardcoreModeActive()) {
-			if (g_Config.bAchievementsSaveStateInHardcoreMode && ((op.type == SaveState::SAVESTATE_SAVE) || (op.type == SAVESTATE_SAVE_SCREENSHOT))) {
-				// We allow saving in hardcore mode if this setting is on.
-			} else {
-				// Operation not allowed
-				return;
-			}
 		}
 
 		std::lock_guard<std::mutex> guard(mutex);
